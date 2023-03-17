cc848d9bad54750422cff247cbb4017aa53e2f7a
Sebastian Mendez <mendezsebas8@gmail.com>
2023-03-16 22:46:06 -0500

    "Cambios trasladamos componente"

--- Screens/Auth/BeginScreen/BeginScreen.js
+++ Screens/Auth/BeginScreen/BeginScreen.js
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
 
 export default function BeginScreen() {
   return (
-    <View>
-      <Text>BeginScreen</Text>
-    </View>
+    <ImageBackground
+      source={require('../../../assets/Bienvenida-Ingreso/BG-Menú-Ingresar.png')}
+      style={styles.imageBackground}>
+      <View style={styles.overlay}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Bienvenido a Medical</Text>
+        </View>
+      </View>
+    </ImageBackground>
+
   )
 }
+
+const styles = StyleSheet.create({
+    imageBackground: {
+      flex: 1,
+      resizeMode: 'cover',
+      justifyContent: 'center',
+    },
+    overlay: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: 'rgba(126, 114, 209, 0.8)', // El último número es la opacidad (0-1)
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 16,
+      width: '100%',
+      maxWidth: 500, // Ajusta este valor según tu preferencia
+    },
+    title: { 
+      fontSize: 30.36,
+      fontFamily: 'Montserrat-SemiBold',
+      textAlign: 'center',
+      color: '#FFF',
+      paddingLeft:60,
+      paddingRight:60, 
+    },
+    iconContainer: {
+      marginRight: 10,
+    },
+  });
