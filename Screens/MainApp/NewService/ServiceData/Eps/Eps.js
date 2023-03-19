98712a8856d9bf5b72781441f6d0b67870cbe503
jalmurh <jorge0420@outlook.es>
2023-03-19 12:51:19 -0500

    imagenes a asset y mod de eps

--- Screens/MainApp/NewService/ServiceData/Eps/Eps.js
+++ Screens/MainApp/NewService/ServiceData/Eps/Eps.js
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
 import React from 'react'
 import styles from './EpsStyle'
+import Globalstyles from '../../../../../Shared/Icons/GlobalStyles'
 
-export default function Eps() {
+export default function Eps(props) {
   return (
-    <View>
-      <Text>Eps</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../../../../../assets/Home/BG-Medical.pnng')}
+        style={styles.imageBackground}>
+      </ImageBackground>
+
+      <View style={styles.MainContainer}>
+        <TouchableOpacity style={styles.buttonIn} onPress={() => navigation.navigate('Login')}>
+              <Text style={{...styles.buttonText,...Globalstyles.Medium}}>Siguiente</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- Screens/MainApp/NewService/ServiceData/Eps/EpsStyle.js
+++ Screens/MainApp/NewService/ServiceData/Eps/EpsStyle.js
@@ -1,6 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+  container: {
+    width:'100%',
+    height:'100%',
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    width: '100%',
+    height:'100%',
+  },
   });
 export default styles;
