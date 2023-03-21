6e4324a3426346d6c7a01c27f5c26855e77f9f3f
Sebastian Mendez <mendezsebas8@gmail.com>
2023-03-21 15:02:41 -0500

    "Estructura para conexión de servicios"

---
+++ Appconfig.js
@@ -0,0 +1,4 @@
+export const configuraciones={
+    server: 'http://localhost:8000/api/',
+    //server: 'https://backend-mhc-web.azurewebsites.net/api/'
+}
---
+++ environments/environments.js
@@ -0,0 +1,19 @@
+import { configuraciones } from "../Appconfig";
+
+let server = configuraciones.server;
+
+export const environment = {
+  production: false,
+  // API
+  api: server,
+
+  // SERVICIOS DE GESTIÓN DE USUARIO
+  logIn: 'login/standar_login/',
+  signIn: 'usuarios/create_user/',
+  updateUser: 'usuarios/update_user/',
+  getToken: 'login/response_reset_password/',
+  resetPassword: 'login/reset_password/',
+  changePassword: 'login/change_password/',
+  userInfo: 'usuarios/user_info/',
+  
+}
