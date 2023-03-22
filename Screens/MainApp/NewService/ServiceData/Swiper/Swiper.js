3193bf08eaddc39afd70470e5124c905f977d7ba
Sebastian Mendez <mendezsebas8@gmail.com>
2023-03-22 00:29:25 -0500

    "Terminamos swiper nuevo servicio"

--- Screens/MainApp/NewService/ServiceData/Swiper/Swiper.js
+++ Screens/MainApp/NewService/ServiceData/Swiper/Swiper.js
@@ -1,18 +1,26 @@
-import { View, Text ,FlatList, StyleSheet ,TouchableOpacity} from 'react-native'
-import React from 'react'
+import { View, Text ,FlatList, StyleSheet ,TouchableOpacity, Animated} from 'react-native'
+import React, {useState,useRef} from 'react'
 import Globalstyles from '../../../../../Shared/Icons/GlobalStyles';
 import SwiperData from './SwiperData';
 import SwiperItem  from './SwiperItem';
 import { Icon } from 'react-native-elements';
 import styles from './SwiperStyles';
+import Paginator from './Paginator';
 
 export default function Swiper(props) {
 
   let {navigation}=props;
+
+  /* SCROLL */
+  const scrollX=useRef(new Animated.Value(0)).current;
+
   return (
     <View style={styles2.container  }>
       
-      <FlatList style={{width:'100%'}}  data={SwiperData} renderItem={({item})=><SwiperItem item={item} props={props}></SwiperItem>} horizontal  showsHorizontalScrollIndicator pagingEnabled>Swiper</FlatList>
+      <FlatList onScroll={Animated.event([{nativeEvent:{contentOffset:{x:scrollX}}}],{
+        useNativeDriver:false,
+      })} style={{width:'100%'}}  data={SwiperData} renderItem={({item})=><SwiperItem item={item} props={props}></SwiperItem>} horizontal showsHorizontalScrollIndicator={false}  pagingEnabled>Swiper</FlatList>
+      <Paginator data={SwiperData} scrollX={scrollX}></Paginator>
       <TouchableOpacity style={{...styles.button,position:'absolute',top:'87%'}} onPress={() => navigation.navigate('NewServices')}>
               <Icon name="chevron-right" type="font-awesome" size={20} color="#fff" style={{position:'relative'}} />
               <Text style={{...styles.buttonText,...Globalstyles.Medium,}}>Siguiente</Text>
